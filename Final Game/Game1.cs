b358768c09746454d4e139af31a2630837fe58fc
Cole Cantwell <CantCole333@GoTVDSB.ca>
2020-11-09 13:14:09 -0500

    moving player

--- Final Game/Game1.cs
+++ Final Game/Game1.cs
@@ -6,8 +6,13 @@ namespace Final_Game
 {
     public class Game1 : Game
     {
+        
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Texture2D playerTexture;
+        Rectangle playerRect;
+
 
         public Game1()
         {
@@ -20,6 +25,8 @@ namespace Final_Game
         {
             // TODO: Add your initialization logic here
 
+            playerRect = new Rectangle(300, 300, 50, 50);
+
             base.Initialize();
         }
 
@@ -28,14 +35,32 @@ namespace Final_Game
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            playerTexture = Content.Load<Texture2D>("Player still");
+            
         }
 
         protected override void Update(GameTime gameTime)
         {
+            KeyboardState state = Keyboard.GetState();
+
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
+            if (state.IsKeyDown(Keys.Left))
+            {
+                playerRect.X -= 3;
+                playerTexture = Content.Load<Texture2D>("Player running");
+
+            }
+            if (state.IsKeyDown(Keys.Right))
+            {
+                playerRect.X += 3;
+                playerTexture = Content.Load<Texture2D>("Player running");
+
+            }
+            
 
             base.Update(gameTime);
         }
@@ -45,6 +70,10 @@ namespace Final_Game
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(playerTexture, playerRect, Color.White);
+            _spriteBatch.End();
+
 
             base.Draw(gameTime);
         }
